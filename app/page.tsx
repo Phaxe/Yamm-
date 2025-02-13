a3f4e8cf7aef37b9d835b37902ae3e5a983e23a0
Phaxe <Yahia.eid@outlook.com>
2025-02-13 02:34:47 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="mx-auto w-64 h-64 text-center my-10">DashBoard</div>
+    <div className="mx-auto w-64 h-64 text-center font-semibold my-10">DashBoard</div>
   );
 }
